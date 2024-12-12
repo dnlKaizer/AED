940dded55257be61597fdf2a384b28d374c23df5
dnlKaizer <danilokaizer@gmail.com>
2024-12-12 18:30:27 -0300

    add: projeto java

---
+++ Peano/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Lista<Integer> list = new Lista<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+    }
+}

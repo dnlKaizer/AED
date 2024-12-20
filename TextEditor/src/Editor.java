8580606821393e56337c5c818051ca97361422f1
dnlKaizer <danilokaizer@gmail.com>
2024-12-20 10:57:53 -0300

    add: métodos read() e stackToString() + privando atributos

--- TextEditor/src/Editor.java
+++ TextEditor/src/Editor.java
@@ -1,12 +1,70 @@
 public class Editor {
-    String text;
-    Stack<Character> charStack;
-    Stack<String> lineStack;
+    private String textFile;
+    private Stack<Character> stack;
 
-    public Editor(String text, int linesLimit) {
-        this.text = text;
-        this.charStack = new Stack<>(70);
-        this.lineStack = new Stack<>(linesLimit);
+    private final char CANCEL_CHAR = '#';
+    private final char CANCEL_LINE = '\\';
+    private final char BREAK_LINE = '*';
+    private final char BREAK_TEXT = '~';
+
+    public Editor(String textFile) {
+        this.textFile = textFile;
+        this.stack = new Stack<>(70);
     }
+
+    public String read() {
+        String text = "";
+        int index = 0;
+        GetText:
+        while (true) {
+            Loop:
+            for (; ; index++) {
+                if (index >= textFile.length()) {
+                    text += System.lineSeparator() + stackToString();
+                    break GetText;
+                }
+                char character = textFile.charAt(index);
+
+                if (character == BREAK_LINE) {
+                    index++;
+                    break Loop;
+                }
+
+                if (character == CANCEL_LINE) {
+                    stack = new Stack<>(70);
+                    index++;
+                    continue GetText;
+                }
+
+                if (character == CANCEL_CHAR) {
+                    try {
+                        stack.pop();
+                    } catch (Exception e) {}
+                    continue Loop;
+                }
+
+                if (character == BREAK_TEXT) {
+                    text += System.lineSeparator() + stackToString();
+                    break GetText;
+                }
+    
+                try {
+                    stack.push(character);
+                } catch(Exception e) {}
+            }
+            text += stackToString() + System.lineSeparator();
+        }
+        return text;
+    }
+
+    public String stackToString() {
+        String line = "";
+        while (stack.size() > 0) {
+            try {
+                line = stack.pop() + line;
+            } catch(Exception e) {}
+        }
+        return line;
+    } 
 
 }

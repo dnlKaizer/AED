e548838af7fab2534c1b67522638a260f79fb876
dnlKaizer <danilokaizer@gmail.com>
2024-12-20 11:11:20 -0300

    del: removendo a classe Conversor e mantendo tudo na main

--- TextEditor/src/App.java
+++ TextEditor/src/App.java
@@ -1,9 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Conversor cv = new Conversor("text.txt");
-        Editor editor = new Editor(cv.readText());
-        String text = editor.read();
+        Editor editor = new Editor(readFile("text.txt"));
         System.out.println();
-        System.out.println(text);
+        System.out.println();
+        System.out.println(editor.read());
+    }
+
+    public static String readFile(String url) {
+        String text = "";
+        try (BufferedReader br = new BufferedReader(new FileReader(url))) {
+            text = br.readLine();
+            if (text == null) text = "";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text;
     }
 }

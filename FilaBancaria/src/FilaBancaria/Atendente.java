fec3d92522045915111e6040758034f1f6a8ea6c
dnlKaizer <danilokaizer@gmail.com>
2025-01-22 14:37:46 -0300

    add: classe Atendente

---
+++ FilaBancaria/src/FilaBancaria/Atendente.java
@@ -0,0 +1,52 @@
+package FilaBancaria;
+
+public class Atendente {
+    private final int TEMPO_MAX_TRABALHO = 180;
+    private final int TEMPO_DESCANSO = 30;
+
+    private int qtdAtendimentos;
+    private int cargaHorariaAtendimentos;
+
+    private int tempoAtendimento;
+    private int tempoDescanso;
+
+    public Atendente() {
+        this.qtdAtendimentos = 0;
+        this.cargaHorariaAtendimentos = 0;
+        this.tempoAtendimento = 0;
+        this.tempoDescanso = 0;
+    }
+
+    public int getQtdAtendimentos() {
+        return qtdAtendimentos;
+    }
+
+    public int cargaHorariaAtendimentos() {
+        return cargaHorariaAtendimentos;
+    }
+
+    public void atender() {
+        tempoAtendimento++;
+        cargaHorariaAtendimentos++;
+    }
+
+    public void descansar() {
+        tempoDescanso++;
+    }
+
+    public void irDescansar() {
+        tempoAtendimento = 0;
+    }
+
+    public void irTrabalhar() {
+        tempoDescanso = 0;
+    }
+
+    public boolean acabouTurno() {
+        return tempoAtendimento > TEMPO_MAX_TRABALHO;
+    }
+
+    public boolean estaDescansado() {
+        return tempoDescanso > TEMPO_DESCANSO; 
+    }
+}

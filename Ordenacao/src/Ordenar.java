a9b481a4db6b82ab0b84302865e143dff7b16d61
dnlKaizer <danilokaizer@gmail.com>
2025-01-23 10:21:34 -0300

    add: métodos da inserção direta e shell sort

--- Ordenacao/src/Ordenar.java
+++ Ordenacao/src/Ordenar.java
@@ -30,4 +30,42 @@ public class Ordenar {
             vet[i] = aux;
         }
     }
+
+    public void insercaoDireta(Item[] vet) {
+        if (vet == null) throw new IllegalArgumentException("Vetor nulo");
+        int i, j, n = vet.length;
+        Item aux;
+        for (i = 1; i < n; i++) {
+            aux = vet[i];
+            j = i - 1;
+            while ((j >= 0) && (aux.compara(vet[j]) < 0)) {
+                vet[j + 1] = vet[j];
+                j--;
+            }
+            vet[j + 1] = aux;
+        }
+    }
+
+    public void shellSort(Item[] vet) {
+        if (vet == null) throw new IllegalArgumentException("Vetor nulo");
+        int n = vet.length;
+        int i, j;
+        int h = 1;
+        Item aux;
+        do {
+            h = h * 3 + 1;
+        } while (h < n);
+        do {
+            h /= 3;
+            for (i = h; i < n; i++) {
+                aux = vet[i];
+                j = i;
+                while ((j >= h) && aux.compara(vet[j - h]) < 0) {
+                    vet[j] = vet[j - h];
+                    j -= h;
+                }
+                vet[j] = aux;
+            }
+        } while (h != 1);
+    }
 }

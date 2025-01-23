3fa453c211094ac6acf10b348e087a371cf1193b
dnlKaizer <danilokaizer@gmail.com>
2025-01-23 10:41:09 -0300

    add: método setChave

--- Ordenacao/src/Dado.java
+++ Ordenacao/src/Dado.java
@@ -11,6 +11,11 @@ public class Dado implements Item {
     }
 
     @Override
+    public void setChave(int chave) {
+        this.chave = chave;
+    }
+
+    @Override
     public int compara(Item item) {
         if (this.chave < item.getChave()) return -1;
         else if (this.chave > item.getChave()) return 1;
